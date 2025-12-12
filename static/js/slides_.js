b85504f07c8f3c544e8713ab16989fcd711d9f2d
Justus <justus.d2025@gmail.com>
2025-12-12 03:04:51 +0100

    nested projects in slideshow-container

--- static/js/slides_.js
+++ static/js/slides_.js
@@ -1,8 +1,7 @@
 let slideIndex = 1;
 const slides = document.getElementsByClassName("slide");
 const dots = document.getElementsByClassName("dot");
-console.log(slides)
-console.log(dots)
+
 showSlides(slideIndex);
 
 function currentSlide(n) {
@@ -25,4 +24,33 @@ function showSlides(n) {
     // sets the selected image as active
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
+}
+
+
+let projectIndex = 1;
+const projects = document.getElementsByClassName("project");
+const pdots = document.getElementsByClassName("pdot");
+
+showProjects(projectIndex);
+
+function currentProject(n) {
+    showProjects(projectIndex = n);
+}
+
+function plusProjects(n) {
+    showProjects(projectIndex += n);
+}
+
+function showProjects(n) {
+    let i;
+    
+    if (n > projects.length) projectIndex = 1;
+    if (n < 1) projectIndex = projects.length;
+    // reset the slides
+    for (i = 0; i < projects.length; i++) projects[i].style.display = "none";
+    // reset the dots
+    for (i = 0; i < pdots.length; i++) pdots[i].className = pdots[i].className.replace(" active", "");
+    // sets the selected image as active
+    projects[projectIndex-1].style.display = "block";
+    pdots[projectIndex-1].className += " active";
 }
