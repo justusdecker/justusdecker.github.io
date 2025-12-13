3fefdd7d9bc6f490a31dc0a96f235aeea47d9da1
Justus <justus.d2025@gmail.com>
2025-12-14 00:08:16 +0100

    v1 completed, refac incoming

--- static/js/slides_.js
+++ static/js/slides_.js
@@ -1,6 +1,5 @@
 let slideIndex = 1;
 const slides = document.getElementsByClassName("slide");
-const dots = document.getElementsByClassName("dot");
 
 showSlides(slideIndex);
 
@@ -19,17 +18,14 @@ function showSlides(n) {
     if (n < 1) slideIndex = slides.length;
     // reset the slides
     for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
-    // reset the dots
-    for (i = 0; i < dots.length; i++) dots[i].className = dots[i].className.replace(" active", "");
     // sets the selected image as active
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+
 }
 
 
 let projectIndex = 1;
 const projects = document.getElementsByClassName("project");
-const pdots = document.getElementsByClassName("pdot");
 
 showProjects(projectIndex);
 
@@ -48,11 +44,8 @@ function showProjects(n) {
     if (n < 1) projectIndex = projects.length;
     // reset the slides
     for (i = 0; i < projects.length; i++) projects[i].style.display = "none";
-    // reset the dots
-    for (i = 0; i < pdots.length; i++) pdots[i].className = pdots[i].className.replace(" active", "");
     // sets the selected image as active
     projects[projectIndex-1].style.display = "block";
-    pdots[projectIndex-1].className += " active";
 }
 
 setInterval(function () {
