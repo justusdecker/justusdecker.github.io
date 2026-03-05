a48a04eaf700d3337a6c076a90831ddda4f62792
Justus <justus.d2025@gmail.com>
2026-03-05 22:07:52 +0100

    cleaned up repo for migrating the gitlab project

--- static/js/skill_info.js
+++
@@ -1,9 +0,0 @@
-function open_close(id) {
-    const object = document.getElementById(id);
-    if (object.style.height == 0) object.style.height = '0px';
-    if (object.style.height == '0px') {
-        object.style.height = '5.5em';
-    } else {
-        object.style.height = '0px';
-    }
-}
