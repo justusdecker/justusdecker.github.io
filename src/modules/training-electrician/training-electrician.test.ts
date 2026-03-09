231d4835720763e15248c9724234d29b76291a74
Justus <justus.d2025@gmail.com>
2026-03-09 15:57:20 +0100

    Add tests

---
+++ src/modules/training-electrician/training-electrician.test.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest';
+import { trainingElectricianRoutes } from './training-electrician.routes';
+
+describe('GET /training-electrician', () => {
+  it('Enpoint (/training-electrician) - register check', () => {
+    expect(trainingElectricianRoutes.path).toBe('/training-electrician');
+  });
+   
+  it('(/training-electrician) should not throw 404', () => {
+    expect(trainingElectricianRoutes).toBeDefined();
+    expect(trainingElectricianRoutes).not.toBeNull();
+  });
+
+  it('(/training-electrician) should have a valid component [HTML/TSX] attached to the route', () => {
+    expect(trainingElectricianRoutes.element).toBeDefined();
+    expect(trainingElectricianRoutes.element).not.toBeNull();
+  });
+  const paths = ['art', 'craft', 'dev']
+  for (const id in paths) {
+    it(`Webpage-data ACCESS for training-electrician:  /${paths[id]}`, async () => {
+      const result = await fetch(`https://raw.githubusercontent.com/justusdecker/webpage-data/main/portfolio/${paths[id]}/index.json`)
+      
+      expect(result.status).toBe(200);
+    });
+  }
+  
+
+  
+
+});
