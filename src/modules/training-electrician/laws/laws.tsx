d2bb61ac6ddcf985e718d6e56a2b60ef5a7cf9ff
Justus <justus.d2025@gmail.com>
2026-03-09 18:21:39 +0100

    fixed the last issues

--- src/modules/training-electrician/laws/laws.tsx
+++ src/modules/training-electrician/laws/laws.tsx
@@ -1,10 +1,15 @@
 import '../../common/ibox_and_topics.css';
-import { SingleMarkdownLoader } from '../../common/backendFetch';
+import { MarkdownLoader } from '../../common/markdownLoader';
 import { GitHubRawBaseUrl } from '../../common/constants';
-
+export interface PortfolioEntryContent {
+    body: string;
+}
 export default function Laws() {
 
   return (
-    <SingleMarkdownLoader url = {`${GitHubRawBaseUrl}Elekro-Ausbildung-Lernstoff/main/laws.md`}/>
-  );
+    <>
+        <MarkdownLoader url = {`${GitHubRawBaseUrl}Elekro-Ausbildung-Lernstoff/main/laws.md`}/>
+    </>
+    
+);
 }
