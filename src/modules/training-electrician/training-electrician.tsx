36631c18904c9e820a4744f19fa53f7f20bc11fa
Justus <justus.d2025@gmail.com>
2026-03-10 10:42:20 +0100

    .

---
+++ src/modules/training-electrician/training-electrician.tsx
@@ -0,0 +1,19 @@
+
+import { NavLink } from 'react-router-dom';
+
+import '../common/ibox_and_topics.css'
+
+export function TrainingElecticianMain() {
+    return (
+        <div className="portfolio-list">
+            <div className='topic'>
+                <NavLink to="/training-electrician/weekly-summary">Wöchentliche Zusammenfassung</NavLink>
+                <br />
+                <NavLink to="/training-electrician/laws">Wichtige Gesetze</NavLink>
+                <br />
+                <NavLink to="/training-electrician/glossar">Glossar</NavLink>
+            </div>
+        </div>
+    )
+
+}
