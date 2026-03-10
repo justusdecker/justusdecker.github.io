cf691abd7d67d56892384c185308eee994ba4efd
Justus <justus.d2025@gmail.com>
2026-03-10 13:52:09 +0100

    add category - switch to SummaryA4Page

--- src/modules/training-electrician/common/summary.tsx
+++ src/modules/training-electrician/common/summary.tsx
@@ -4,12 +4,12 @@ import { useParams } from 'react-router-dom';
 
 import '../../common/DinA4.css'
 
-export function SummaryA4Page() {
+export function SummaryA4Page({category}:  {category: string}) {
   const { id } = useParams<{ id: string }>();
     return (
         <div className="portfolio-list">
             <div className='a4-page'>
-                <MarkdownLoader url={`https://raw.githubusercontent.com/justusdecker/Elekro-Ausbildung-Lernstoff/main/summary/weekly/${id}.md`} />
+                <MarkdownLoader url={`https://raw.githubusercontent.com/justusdecker/Elekro-Ausbildung-Lernstoff/main/summary/${category}/${id}.md`} />
             </div>
         </div>
     )
