a4efa42537ef9d4fa0ada3e1b7766cb2e69045de
Justus <justus.d2025@gmail.com>
2026-03-10 19:37:22 +0100

    changed navlinks to neo-btn

--- src/modules/training-electrician/training-electrician.tsx
+++ src/modules/training-electrician/training-electrician.tsx
@@ -2,22 +2,22 @@
 import { NavLink } from 'react-router-dom';
 
 import '../common/ibox_and_topics.css'
-
+import '../common/neo-btn.css'
 export function TrainingElecticianMain() {
     return (
         <div className="portfolio-list">
             <div className='topic'>
-                <NavLink to="/training-electrician/weekly-summary">Wöchentliche Zusammenfassung</NavLink>
-                <br />
-                <NavLink to="/training-electrician/monthly-summary">Monatliche Zusammenfassung</NavLink>
-                <br />
-                <NavLink to="/training-electrician/quarterly-summary">Quartal Zusammenfassung</NavLink>
-                <br />
-                <NavLink to="/training-electrician/exam-preperation">Prüfungsvorbereitung</NavLink>
-                <br />
-                <NavLink to="/training-electrician/laws">Wichtige Gesetze</NavLink>
-                <br />
-                <NavLink to="/training-electrician/glossar">Glossar</NavLink>
+                <NavLink className='neo-btn' to="/training-electrician/weekly-summary">Wöchentliche Zusammenfassung</NavLink>
+                <br /><br />
+                <NavLink className='neo-btn' to="/training-electrician/monthly-summary">Monatliche Zusammenfassung</NavLink>
+                <br /><br />
+                <NavLink className='neo-btn' to="/training-electrician/quarterly-summary">Quartal Zusammenfassung</NavLink>
+                <br /><br />
+                <NavLink className='neo-btn' to="/training-electrician/exam-preperation">Prüfungsvorbereitung</NavLink>
+                <br /><br />
+                <NavLink className='neo-btn' to="/training-electrician/laws">Wichtige Gesetze</NavLink>
+                <br /><br />
+                <NavLink className='neo-btn' to="/training-electrician/glossar">Glossar</NavLink>
             </div>
         </div>
     )
