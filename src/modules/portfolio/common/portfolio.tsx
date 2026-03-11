fb62e008ec94f3373f78c2ba1ec06b1f4272fd45
Justus <justus.d2025@gmail.com>
2026-03-11 11:41:05 +0100

    add pagination

--- src/modules/portfolio/common/portfolio.tsx
+++ src/modules/portfolio/common/portfolio.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import type { PortfolioMetadata } from "./portfolio.types";
-import { Link } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 
 import '../../common/ibox_and_topics.css'
@@ -26,32 +25,58 @@ function PortfolioDetail(attr: { category: string, id: string }) {
       <div className="portfolio-detail "><ReactMarkdown>{content}</ReactMarkdown></div>
   );
 }
-
+const POSTS_PER_PAGE = 10;
 export default function PortfolioIndex({ category }: { category: string }) {
   const [posts, setPosts] = useState<PortfolioMetadata[]>([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
     fetch(`https://raw.githubusercontent.com/justusdecker/webpage-data/main/portfolio/${category}/index.json`)
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, [category]);
   console.log(posts)
+
+  const categorys_en = ['dev', 'craft', 'art'];
+  const categorys_de = ['Software', 'Handwerk', 'Kunst']
+  const cat_id = categorys_en.indexOf(category);
+  const category_head = categorys_de[cat_id];
+
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
+  const startIndex = (currentPage - 1) * POSTS_PER_PAGE;
+  const filteredPosts = posts.slice(startIndex, startIndex + POSTS_PER_PAGE);
+
   return (
     <div className="portfolio-list">
-      <h1>Portfolio: {category} Übersicht</h1>
+      <h1>Portfolio: {category_head} Übersicht</h1>
       
-        {posts.map((post) => (
+        {filteredPosts.map((post) => (
             <div className='topic-item'>
             <div key={post.id}>
-                <Link to={`/portfolio/${category}/${post.id}`}>
                 <h2>{post.title}</h2>
                 <p>{post.timespan}</p>
                 <PortfolioDetail category={category} id={post.id}/>
-                </Link>
             </div>
             </div>
         ))}
-      
+      <div className="pagination">
+        <button 
+          disabled={currentPage === 1} 
+          onClick={() => setCurrentPage(prev => prev - 1)}
+          className='neo-btn'
+        >
+          Zurück
+        </button>
+        
+        <span style={{paddingLeft:"1rem", paddingRight:"1rem"}}>Seite {currentPage} von {totalPages || 1}</span>
+        
+        <button 
+          disabled={currentPage >= totalPages} 
+          onClick={() => setCurrentPage(prev => prev + 1)}
+          className='neo-btn'
+        >
+          Weiter
+        </button>
+      </div>
     </div>
   );
 }
