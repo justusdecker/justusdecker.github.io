cbd243103a9784559094ad98b848e3761ae9a673
Justus <justus.d2025@gmail.com>
2026-03-16 12:18:43 +0100

    add impressum

--- src/modules/contact/contact.routes.tsx
+++ src/modules/contact/contact.routes.tsx
@@ -1,26 +1,27 @@
 import { type RouteObject } from 'react-router-dom';
 import Header from '../common/header';
-import '../common/ibox_and_topics.css'
+import '../common/ibox_and_topics.css';
 export const contactRoutes: RouteObject = {
     path: "/contact",
     element: (
        <>
             <Header />
-            <section className="info-box info-box-overwrite">
-                <div className="topic-list">
-                <div className="topic-item">
-                    <strong>💻 Email</strong>
+            <section>
+                <div className="topic">
+                    
+                    <h2>Impressum</h2>
+                    
+                    <div>
+                        <i>Angaben gem. §5 TMG:</i><br /><br />
+                        <span>Justus Decker</span><br />
+                        <span>Schwabenstraße 14</span><br />
+                        <span>26723 Emden</span><br /><br />
+                    </div>
+                    <strong>Kontakt:</strong>
                     <p><a href="mailto:justus.d2025@gmail.com">justus.d2025@gmail.com</a></p>
+                    <p><a href="https://www.linkedin.com/in/justus-decker/">LinkedIn</a></p>
                 </div>
-                <div className="topic-item">
-                    <strong>LinkedIn</strong>
-                    <p><a href="https://www.linkedin.com/in/justus-decker/">justus-decker</a></p>
-                </div>
-                <div className="topic-item">
-                    <strong>Kontaktformular</strong>
-                    <p>Diese Funktion ist aktuell noch nicht implementiert!</p>
-                </div>
-                </div>
+                
             </section>
        </> 
     )
