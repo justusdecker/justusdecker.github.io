a47270f2d236df7a88e32faab6c4c85773e8f89d
Justus <justus.d2025@gmail.com>
2026-08-02 12:57:11 +0200

    removed blog

--- src/modules/blog/blog.constants.tsx
+++
@@ -1,9 +0,0 @@
-import { GitHubRawBaseUrl } from "../common/constants";
-
-export const getBlogIndexUrl = (): string => {
-  return `${GitHubRawBaseUrl}webpage-data/main/posts/index.json`;
-};
-
-export const getBlogPostUrl = (id: string): string => {
-  return `${GitHubRawBaseUrl}webpage-data/main/posts/${id}.md`;
-};
