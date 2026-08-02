b93f59abaed81c19f97f248065ac3def321272cc
Justus <justus.d2025@gmail.com>
2026-08-02 12:59:48 +0200

    removed unused navlinks and removed image

--- src/modules/common/header.tsx
+++ src/modules/common/header.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 import './header.css'
+import { Canvas } from "./canvas";
 const Header: React.FC = () => {
   return (
     <header id="head">
       <div className="head-bg"></div>
-
-      <img 
-        id="profile" 
-        src="https://avatars.githubusercontent.com/u/200506279?v=4" 
-        alt="Justus Decker Profilbild" 
-      />
-      
       <div id="head-text">
         <h1>Justus Decker</h1>
         <h2>Handwerker & Techniker</h2>
@@ -23,22 +17,7 @@ const Header: React.FC = () => {
           >
             Start
           </NavLink>
-
-          <div className="dropdown">
-            <span className="dropbtn">Portfolio ▾</span>
-            <div className="dropdown-content">
-              <NavLink to="/portfolio/craft">Handwerk</NavLink>
-              <NavLink to="/portfolio/dev">Software</NavLink>
-              <NavLink to="/portfolio/art">Kunst</NavLink>
-            </div>
-          </div>
-
-          <NavLink 
-            to="/blog" 
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            Blog
-          </NavLink>
+          
 
           
           <div className="dropdown">
@@ -47,19 +26,6 @@ const Header: React.FC = () => {
               <NavLink to="/contact">Kontakt</NavLink>
               <NavLink to="/cv">CV</NavLink>
               <NavLink to="/certificates">Zertifikate</NavLink>
-            </div>
-          </div>
-          <div className="dropdown">
-            <span className="dropbtn">Umschulung Elektroniker ▾</span>
-            <div className="dropdown-content">
-              <NavLink to="/training-electrician/weekly-summary">Wochen-Zusammenfassung</NavLink>
-              <NavLink to="/training-electrician/monthly-summary">Monat-Zusammenfassung</NavLink>
-              <NavLink to="/training-electrician/quarterly-summary">Quartal Zusammenfassung</NavLink>
-              <NavLink to="/training-electrician/exam-preperation">Prüfungsvorbereitung</NavLink>
-              <NavLink to="/training-electrician/glossar">Glossar</NavLink>
-              <NavLink to="/training-electrician/laws">Gesetze</NavLink>
-              <NavLink to="/training-electrician/formulars-math">Matheformeln</NavLink>
-              <NavLink to="/training-electrician/timetable">Stundenplan</NavLink>
             </div>
           </div>
           <NavLink 
