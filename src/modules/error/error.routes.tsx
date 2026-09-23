d2b09efb35928f0b6e68a80897535e85c78cf07a
Justus <justus.d2025@gmail.com>
2026-09-23 23:33:01 +0200

    switched to MsgBox

--- src/modules/error/error.routes.tsx
+++ src/modules/error/error.routes.tsx
@@ -1,11 +1,13 @@
+import { MsgBox } from "../common/msgbox";
+
 export default function ErrorPage() {
   return (
-    <div className="error-container">
-      <h1>Upps! Da ist etwas schiefgelaufen.</h1>
-      <p>Die gewünschte Seite ist nicht im System oder es gab ein technisches Problem.</p>
+    <MsgBox type="err">
+      
+      <p>Upps! Da ist etwas schiefgelaufen. Die gewünschte Seite ist nicht im System oder es gab ein technisches Problem.</p>
       <button onClick={() => window.location.href = '/'}>
         Hier gehts zurück
       </button>
-    </div>
+    </MsgBox>
   );
 };
